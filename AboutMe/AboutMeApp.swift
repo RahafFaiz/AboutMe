652473fb79d87a8d4c090e0c058fb999b6803248
R <raghad.fayez1@gmail.com>
2023-01-29 23:05:24 +0300

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Raghad Fayez on 07/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AboutMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AboutMe
+//
+//  Created by Raghad Fayez on 07/07/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
